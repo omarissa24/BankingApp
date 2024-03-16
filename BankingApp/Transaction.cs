9b229fd2b3ed5c448578ba6bed30a9456dff396b
Marwan Mai <emef4z@gmail.com>
2024-03-16 22:31:23 +0100

    Add Trancation model

---
+++ BankingApp/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp
+{
+    internal class Transaction
+    {
+        public int transactionId { get; set; }
+        public int accountId { get; set; }
+        public DateTime transactionTime { get; set; }
+        public string transactionType { get; set; }
+        public float tranactionAmount { get; set; }
+        public string transactionStatus { get; set; }
+    }
+}
