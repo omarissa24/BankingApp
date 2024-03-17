d7227489443d7096d21d634abc7ed8e39c84f571
omar <oissa2424@gmail.com>
2024-03-17 01:52:18 +0100

    Added functionality for deposit and View Balance forms

--- BankingApp/DepositForm.Designer.cs
+++ BankingApp/DepositForm.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DepositForm";
+            depositAmountLabel = new Label();
+            depositAmountTextBox = new TextBox();
+            depositButton = new Button();
+            SuspendLayout();
+            // 
+            // depositAmountLabel
+            // 
+            depositAmountLabel.AutoSize = true;
+            depositAmountLabel.Location = new Point(424, 216);
+            depositAmountLabel.Name = "depositAmountLabel";
+            depositAmountLabel.Size = new Size(236, 41);
+            depositAmountLabel.TabIndex = 0;
+            depositAmountLabel.Text = "Deposit Amount";
+            // 
+            // depositAmountTextBox
+            // 
+            depositAmountTextBox.Location = new Point(361, 331);
+            depositAmountTextBox.Name = "depositAmountTextBox";
+            depositAmountTextBox.Size = new Size(358, 47);
+            depositAmountTextBox.TabIndex = 1;
+            // 
+            // depositButton
+            // 
+            depositButton.BackColor = Color.Blue;
+            depositButton.ForeColor = Color.White;
+            depositButton.Location = new Point(410, 470);
+            depositButton.Name = "depositButton";
+            depositButton.Size = new Size(250, 105);
+            depositButton.TabIndex = 2;
+            depositButton.Text = "Deposit";
+            depositButton.UseVisualStyleBackColor = false;
+            depositButton.Click += depositButton_Click;
+            // 
+            // DepositForm
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1157, 784);
+            Controls.Add(depositAmountLabel);
+            Controls.Add(depositAmountTextBox);
+            Controls.Add(depositButton);
+            Name = "DepositForm";
+            Text = "DepositForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label depositAmountLabel;
+        private TextBox depositAmountTextBox;
+        private Button depositButton;
     }
 }
--- BankingApp/DepositForm.cs
+++ BankingApp/DepositForm.cs
@@ -7,20 +7,143 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace BankingApp
 {
     public partial class DepositForm : Form
     {
+
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private int userId;
+        private int accountId;
+
+
         public DepositForm()
         {
             InitializeComponent();
+            server = "13.39.79.161";
+            database = "bankapp";
+            uid = "epita";
+            password = "Secret@123";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
         }
 
         public DepositForm(int userId)
         {
             InitializeComponent();
-            // Load user data and transactions.
+            this.userId = userId;
+            server = "13.39.79.161";
+            database = "bankapp";
+            uid = "epita";
+            password = "Secret@123";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+            this.accountId = GetAccountId();
+
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            // Get deposit amount
+            float depositAmount = GetDepositAmount();
+            if (depositAmount == 0)
+            {
+                return;
+            }
+
+            // Get current balance
+            float balance = GetBalance();
+
+            // Update balance
+            float newBalance = balance + depositAmount;
+
+            UpdateBalance(newBalance);
+
+            // Add transaction
+            AddTransaction(depositAmount);
+
+            MessageBox.Show("Deposit successful. New balance: " + newBalance);
+        }
+
+        private float GetDepositAmount()
+        {
+            float depositAmount = 0;
+            if (float.TryParse(depositAmountTextBox.Text, out depositAmount))
+            {
+                return depositAmount;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid amount.");
+                return 0;
+            }
+        }
+
+        private float GetBalance()
+        {
+            connection.Open();
+            string query = "SELECT Balance FROM Account WHERE accountUser=@userId";
+            
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@userId", this.userId);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            float balance = reader.GetFloat("Balance");
+            reader.Close();
+            connection.Close();
+
+            return balance;
+        }
+
+        private void UpdateBalance(float newBalance)
+        {
+            connection.Open();
+            string query = "UPDATE Account SET Balance=@newBalance WHERE accountUser=@userId";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@newBalance", newBalance);
+            cmd.Parameters.AddWithValue("@userId", this.userId);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void AddTransaction(float depositAmount)
+        {
+            connection.Open();
+            string query = "INSERT INTO Transaction (idAccount, transactionTime, transactionType, transactionAmount, transactionStatus) VALUES (@idAccount, @transactionTime, @transactionType, @transactionAmount, @transactionStatus)";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+
+            cmd.Parameters.AddWithValue("@idAccount", this.accountId);
+            cmd.Parameters.AddWithValue("@transactionTime", DateTime.Now);
+            cmd.Parameters.AddWithValue("@transactionType", "Deposit");
+            cmd.Parameters.AddWithValue("@transactionAmount", depositAmount);
+            cmd.Parameters.AddWithValue("@transactionStatus", "Success");
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private int GetAccountId()
+        {
+            connection.Open();
+            string query = "SELECT idAccount FROM Account WHERE accountUser=@userId";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@userId", this.userId);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            int accountId = reader.GetInt32("idAccount");
+            reader.Close();
+            connection.Close();
+
+            return accountId;
         }
     }
 }
