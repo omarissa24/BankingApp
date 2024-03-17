3b30b38f0f33e274261738cb66e6ae3837c77879
omar <oissa2424@gmail.com>
2024-03-17 02:03:01 +0100

    withdraw form skeleton

--- BankingApp/WithdrawForm.Designer.cs
+++ BankingApp/WithdrawForm.Designer.cs
@@ -28,17 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            withdrawAmountLabel = new Label();
+            withdrawAmountTextBox = new TextBox();
+            withdrawButton = new Button();
             SuspendLayout();
+
+            //
+            // withdrawAmountLabel
+            //
+
+            withdrawAmountLabel.AutoSize = true;
+            withdrawAmountLabel.Location = new Point(424, 216);
+            withdrawAmountLabel.Name = "withdrawAmountLabel";
+            withdrawAmountLabel.Size = new Size(236, 41);
+            withdrawAmountLabel.TabIndex = 0;
+            withdrawAmountLabel.Text = "Withdraw Amount";
+            //
+            // withdrawAmountTextBox
+            //
+            withdrawAmountTextBox.Location = new Point(361, 331);
+            withdrawAmountTextBox.Name = "withdrawAmountTextBox";
+            withdrawAmountTextBox.Size = new Size(358, 47);
+            withdrawAmountTextBox.TabIndex = 1;
+
+            //
+            // withdrawButton
+            //
+
+            withdrawButton.BackColor = Color.Blue;
+            withdrawButton.ForeColor = Color.White;
+            withdrawButton.Location = new Point(410, 470);
+            withdrawButton.Name = "withdrawButton";
+            withdrawButton.Size = new Size(250, 105);
+            withdrawButton.TabIndex = 2;
+            withdrawButton.Text = "Withdraw";
+            withdrawButton.UseVisualStyleBackColor = false;
+            withdrawButton.Click += withdrawButton_Click;
             // 
             // WithdrawForm
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1205, 925);
+            ClientSize = new Size(1157, 784);
+            Margin = new Padding(1, 1, 1, 1);
             Name = "WithdrawForm";
             Text = "WithdrawForm";
+            Controls.Add(withdrawAmountLabel);
+            Controls.Add(withdrawAmountTextBox);
+            Controls.Add(withdrawButton);
             ResumeLayout(false);
+            PerformLayout();
         }
+
+        private Label withdrawAmountLabel;
+        private TextBox withdrawAmountTextBox;
+        private Button withdrawButton;
 
         #endregion
     }
--- BankingApp/WithdrawForm.cs
+++ BankingApp/WithdrawForm.cs
@@ -7,20 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace BankingApp
 {
     public partial class WithdrawForm : Form
     {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private int userId;
+        private int accountId;
+
         public WithdrawForm()
         {
             InitializeComponent();
+            server = "13.39.79.161";
+            database = "bankapp";
+            uid = "epita";
+            password = "Secret@123";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
         }
 
         public WithdrawForm(int userId)
         {
             InitializeComponent();
-            // Load user data and transactions.
+            server = "13.39.79.161";
+            database = "bankapp";
+            uid = "epita";
+            password = "Secret@123";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+            this.userId = userId;
+            this.accountId = GetAccountId();
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private int GetAccountId()
+        {
+            connection.Open();
+            string query = "SELECT idAccount FROM Account WHERE accountUser=@userId";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@userId", this.userId);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            int accountId = reader.GetInt32("idAccount");
+            reader.Close();
+            connection.Close();
+
+            return accountId;
         }
     }
 }
